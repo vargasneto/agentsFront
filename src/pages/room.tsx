cbe7f5ac8c6028b3b486c457f8cae1049453288c
Pedro <pedrohjs_vargas@hotmail.com>
2025-07-12 12:42:55 -0300

    feat: add sistemas de rotas e chamada de api

---
+++ src/pages/room.tsx
@@ -0,0 +1,14 @@
+import { useParams, Navigate } from "react-router-dom";
+
+type RoomParams = {
+  roomId: string;
+};
+export function Room() {
+  const params = useParams<RoomParams>();
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  return <div>ROOMDetalhes{JSON.stringify(params)}</div>;
+}
